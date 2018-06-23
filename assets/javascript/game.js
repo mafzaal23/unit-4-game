6977c6b4c4a81b383ca537a8e0807c131a2b1231
mafzaal23 <mafzaal04@gmail.com>
2018-06-22 23:22:27 -0500

    Upload

---
+++ assets/javascript/game.js
@@ -0,0 +1,26 @@
+var crystal = {
+
+    Crystal1: {
+      name: "Crystal1",
+      value: 0
+    },
+  
+    Crystal2: {
+      name: "Crystal2",
+      value: 0
+    },
+  
+    Crystal3: {
+      name: "Crystal3",
+      value: 0
+    },
+  
+    Crystal4: {
+      name: "Crystal4",
+      value: 0
+    }
+  }
+  
+  var getRandom = function(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  };
